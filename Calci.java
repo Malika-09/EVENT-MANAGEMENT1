66387d9aebec6c100ee6512b6daef0ee09acf341
Malika_09 <Malikapinjar@gamil.com>
2018-08-24 15:51:54 +0530

    CALCULATOR JAVA PROGRAM

---
+++ Calci.java
@@ -0,0 +1,17 @@
+import java.io.*;
+ class Calci
+ {
+	public static void main(String args[])
+	{
+	  int num1=10, num2=20;
+	  int sum =num1+num2;
+	  int dif= num1-num2;
+	  int prod=num1*num2;
+	  int quo=num1/num2;
+	  
+	  System.out.println("Diff of num1 and num2 = " +dif);
+	  System.out.println("Prod of num1 and num2 = " +prod);
+	  System.out.println("Quo of num1 and num2 = " +quo);
+	 }
+  }
+  
